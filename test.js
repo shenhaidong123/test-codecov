75c5aecdac0d2ea36bc5959f70a9f86fdec43615
申海东 <shenhaidong@haomo-studio.com>
2017-05-07 05:12:24 +0800

    commit

---
+++ test.js
@@ -0,0 +1,35 @@
+/**
+ * Created by shd on 2017/5/6.
+ */
+var a = 'istanbul';
+function b(){
+  console.log('b');
+}
+function c(i){
+  if(i == 'istanbul'){
+    return true;
+  }else if(i == '123'){
+    return false;
+  }
+}
+c(a);
+
+
+
+
+// function test(a, b){
+//   var c =0;
+//   if(a>0 && b>0){
+//     z=a
+//   }
+//   return a;
+// }
+// int foo (int x, int y)
+// {
+//   int z = 0;
+//   if ((x>0) && (y>0))
+//   {
+//     z = x;
+//   }
+//   return z;
+// }
